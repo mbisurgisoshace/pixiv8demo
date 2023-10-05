4d2553b7d37aafd75ac8141dd9400db5233c9459
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2023-10-05 14:11:54 -0300

    Init commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import * as PIXI from "pixi.js";
+
+let type = "WebGL";
+
+if (!PIXI.utils.isWebGLSupported()) {
+  type = "canvas";
+}
+
+//PIXI.utils.sayHello(type);
+
+const app = new PIXI.Application({
+  backgroundAlpha: 1,
+  width: document.body.clientWidth,
+  height: document.body.clientHeight,
+  antialias: true,
+  resolution: window.devicePixelRatio || 1,
+  resizeTo: window,
+  autoDensity: true,
+});
+
+app.stage.eventMode = "static";
+
+document.body.appendChild(app.view);
+
+const rectangle = new PIXI.Graphics();
+rectangle.lineStyle({ width: 4, color: 0xff3300, alpha: 1 });
+rectangle.beginFill(0x66ccff);
+rectangle.drawRect(0, 0, 64, 64);
+rectangle.endFill();
+rectangle.x = 170;
+rectangle.y = 170;
+app.stage.addChild(rectangle);
+
+for (let i = 0; i < 1000; i++) {
+  const rectangle = new PIXI.Graphics();
+  rectangle.lineStyle({ width: 4, color: 0xff3300, alpha: 1 });
+  rectangle.beginFill(0x66ccff);
+  rectangle.drawRect(0, 0, 64, 64);
+  rectangle.endFill();
+  rectangle.x = app.screen.width * Math.random();
+  rectangle.y = app.screen.height * Math.random();
+  app.stage.addChild(rectangle);
+}
+
+let startPos, lastPos, delta, isKeyDown;
+addEventListener("pointerdown", onDown);
+addEventListener("pointermove", onMove);
+addEventListener("pointerup", onUP);
+addEventListener("wheel", onWheel);
+
+app.stage.on("pointerup", onDragEnd);
+app.stage.on("pointerupoutside", onDragEnd);
+
+function onDown(e) {
+  isKeyDown = true;
+  startPos = { x: e.x, y: e.y };
+  lastPos = null;
+}
+function onMove(e) {
+  if (!isKeyDown) return;
+  if (!lastPos) delta = { x: startPos.x - e.x, y: startPos.y - e.y };
+  else delta = { x: e.x - lastPos.x, y: e.y - lastPos.y };
+  lastPos = { x: e.x, y: e.y };
+  app.stage.x += delta.x;
+  app.stage.y += delta.y;
+}
+function onUP(e) {
+  isKeyDown = false;
+}
+
+const smoothZoom = 60;
+function onWheel(e) {
+  const v = e.deltaY < 0 ? 1 : -1;
+  const s = (v * smoothZoom) / 1000;
+  app.stage.scale.x += s;
+  app.stage.scale.y += s;
+}
---
+++ indexv8.js
@@ -0,0 +1,95 @@
+import { Application, autoDetectRenderer, Graphics } from "pixi.js";
+
+const app = new Application();
+
+(async () => {
+  await app.init({
+    backgroundAlpha: 1,
+    width: document.body.clientWidth,
+    height: document.body.clientHeight,
+    antialias: true,
+    resolution: window.devicePixelRatio || 1,
+    resizeTo: window,
+    autoDensity: true,
+    hello: true,
+  });
+
+  app.stage.eventMode = "static";
+  app.stage.hitArea = app.screen;
+
+  document.body.appendChild(app.canvas);
+
+  for (let i = 0; i < 1000; i++) {
+    const rectangle = new Graphics();
+    rectangle.rect(0, 0, 64, 64);
+    rectangle.fill(0x66ccff);
+    // rectangle.eventMode = "static";
+    // rectangle.on("pointerdown", onDragStart, rectangle);
+    rectangle.stroke({ width: 1, color: "0xff3300" });
+    rectangle.x = app.screen.width * Math.random();
+    rectangle.y = app.screen.height * Math.random();
+    app.stage.addChild(rectangle);
+  }
+
+  // or
+  //const renderer = await autoDetectRenderer({}); // WebGL or WebGPU
+
+  let startPos, lastPos, delta, isKeyDown, dragTarget;
+  app.stage.on("pointerdown", onDown);
+  app.stage.on("pointermove", onMove);
+  app.stage.on("pointerup", onUP);
+  app.stage.on("wheel", onWheel);
+  //app.stage.on("pointerup", onDragEnd);
+  //app.stage.on("pointerupoutside", onDragEnd);
+
+  //   function onDragMove(event) {
+  //     if (dragTarget) {
+  //       dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+  //     }
+  //   }
+
+  //   function onDragStart() {
+  //     this.alpha = 0.5;
+  //     dragTarget = this;
+  //     app.stage.on("pointermove", onDragMove);
+  //   }
+
+  //   function onDragEnd() {
+  //     if (dragTarget) {
+  //       app.stage.off("pointermove", onDragMove);
+  //       dragTarget.alpha = 1;
+  //       dragTarget = null;
+  //     }
+  //   }
+
+  function onDown(e) {
+    isKeyDown = true;
+    startPos = { x: e.x, y: e.y };
+    lastPos = null;
+  }
+  function onMove(e) {
+    if (!isKeyDown) return;
+    if (!lastPos) delta = { x: startPos.x - e.x, y: startPos.y - e.y };
+    else delta = { x: e.x - lastPos.x, y: e.y - lastPos.y };
+    lastPos = { x: e.x, y: e.y };
+    app.stage.x += delta.x;
+    app.stage.y += delta.y;
+  }
+  function onUP(e) {
+    isKeyDown = false;
+
+    if (dragTarget) {
+      app.stage.off("pointermove", onDragMove);
+      dragTarget.alpha = 1;
+      dragTarget = null;
+    }
+  }
+
+  const smoothZoom = 60;
+  function onWheel(e) {
+    const v = e.deltaY < 0 ? 1 : -1;
+    const s = (v * smoothZoom) / 1000;
+    app.stage.scale.x += s;
+    app.stage.scale.y += s;
+  }
+})();
